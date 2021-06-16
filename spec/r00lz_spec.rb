e5b6dfe29b6a106d25280e4e9fd6f1ebd839660f
MishalAfridi <mishal94.ma@gmail.com>
2021-06-16 21:09:55 +0100

    update spec to test controller

--- spec/r00lz_spec.rb
+++ spec/r00lz_spec.rb
@@ -3,8 +3,12 @@ RSpec.describe R00lz do
     expect(R00lz::VERSION).not_to be nil
   end
 
+  class TedController < R00lz::Controller
+    def think; "Whoah, man..."; end 
+  end
+
   it "returns 200" do
-    env = { "PATH_INFO" => "/", "QUERY_STRING" => "" }
+    env = { "PATH_INFO" => "/ted/think", "QUERY_STRING" => "" }
     expect(::R00lz::App.new.call(env)[0]).to eq 200
   end
 end
